fb996eccb69807a6885aa79b0ed9f03df6838342
richardhan <hxy.314@gmail.com>
2019-09-06 23:55:31 +1000

    re

--- client/src/components/AirportList.js
+++ client/src/components/AirportList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {graphql} from 'react-apollo';
 import query from '../queries/query-airports'
 import { Link } from 'react-router-dom';
@@ -7,37 +7,62 @@ import Card from '@material-ui/core/Card';
 import styled from 'styled-components'
 import componentStyle from './AirportListStyle';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import Pagination from "react-js-pagination";
 
-const AirportList = props => {
-  const { error, airports, loading } = props.data;
-  const Wrapper = styled.div`${componentStyle}`;
+class AirportList extends Component { 
+  constructor(props) {
+    super(props);
+    this.state = {
+      activePage: 1
+    };
+    this.handlePageChange = this.handlePageChange.bind(this);
+  }
 
-  if(error) {
-    return <div>{error.message}</div>
+  handlePageChange(pageNumber) {
+    this.setState({activePage: pageNumber});
   }
   
-  if(loading) {
-    return <Loader />
+  render(){
+    const { error, airports, loading } = this.props.data;
+    const Wrapper = styled.div`${componentStyle}`;
+    const totalNumber = airports && airports.length;
+    const currentPage = this.state.activePage * 100
+    const nextPage = currentPage + 100
+
+    const airportsList = airports && airports.slice(currentPage, nextPage).map( airport => {
+        return (
+            <Wrapper key={airport.airportCode} >
+              <Card>
+                  <Link to={`/airport/${airport.airportCode}`} className="AirportList__link"> 
+                    {airport.airportName} - {airport.country.countryName}
+                    <ArrowForwardIosIcon />
+                  </Link>
+              </Card>
+            </Wrapper>
+        )
+    })
+
+    if(error) {
+      return <div>{error.message}</div>
+    }
+    
+    if(loading) {
+      return <Loader />
+    }
+
+    return (
+      <div> 
+        { airportsList }
+        <Pagination
+          activePage={this.state.activePage}
+          itemsCountPerPage={100}
+          totalItemsCount={totalNumber - 100}
+          pageRangeDisplayed={5}
+          onChange={this.handlePageChange}
+        />
+      </div>
+    )
   }
-
-  const airportsList = airports.map( airport => {
-      return (
-          <Wrapper key={airport.airportCode} >
-            <Card>
-                <Link to={`/airport/${airport.airportCode}`} className="AirportList__link"> 
-                  {airport.airportName} - {airport.country.countryName}
-                  <ArrowForwardIosIcon />
-                </Link>
-            </Card>
-          </Wrapper>
-      )
-  })
-
-  return (
-    <div> 
-      { airportsList }
-    </div>
-  )
 }
 
 export default graphql(query)(AirportList);
