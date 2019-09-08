dea3dfec159751d8052cc155986122f364f65cb0
richardhan <hxy.314@gmail.com>
2019-09-08 20:02:20 +1000

    re

--- client/src/components/AirportItem.js
+++ client/src/components/AirportItem.js
@@ -8,30 +8,32 @@ import PropTypes from 'prop-types';
 
 const Wrapper = styled.div`${componentStyle}`;
 const AirportItem = props => { 
-    return (
-        <Wrapper>
-            <Card className="AirportList__card" >
-                <Link to={`/airport/${props.airport.airportCode}`} className="AirportList__link"> 
-                <span className="AirportList__link-airportName">            {props.airport.airportName}
-                </span> - 
-                
-                <span className="AirportList__link-countryName">            {props.airport.country.countryName}
-                </span> 
-                <ArrowForwardIosIcon />
-                </Link>
-            </Card>
-        </Wrapper>
-    )
+  return (
+    <Wrapper>
+      <Card className="AirportList__card" >
+        <Link to={`/airport/${props.airport.airportCode}`} className="AirportList__link"> 
+          <span>
+            <span className="AirportList__link-airportName">            {props.airport.airportName}
+            </span> 
+              -       
+            <span className="AirportList__link-countryName">            {props.airport.country.countryName}
+            </span> 
+          </span>
+          <ArrowForwardIosIcon />
+        </Link>
+      </Card>
+  </Wrapper>
+  )
 };
 
 AirportItem.propTypes = {
-    airport: PropTypes.shape({
-        airportName: PropTypes.string,
-        airportCode: PropTypes.string,
-        country: PropTypes.shape({
-            countryName: PropTypes.string,
-        })
+  airport: PropTypes.shape({
+    airportName: PropTypes.string,
+    airportCode: PropTypes.string,
+    country: PropTypes.shape({
+      countryName: PropTypes.string,
     })
+  })
 };
 
 export default AirportItem;
